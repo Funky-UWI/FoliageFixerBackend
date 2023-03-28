39efacb63804cbc183bbd5cb36bf2e69c192e324
OjoreKanneh <ojore.kanneh@my.uwi.edu>
2023-03-28 10:37:24 +0000

    classify and scans routes

--- App/views/classify.py
+++ App/views/classify.py
@@ -8,6 +8,8 @@ from App.controllers import (
     authenticate, 
 )
 from App.controllers.scan import *
+from App.controllers.classification import *
+from App.controllers.disease_Solution import *
 
 
 classify_views = Blueprint('classify_views', __name__, template_folder='../templates')
@@ -21,21 +23,34 @@ def test():
 # @jwt_required()
 def upload_scan():
    
-    data = request.json
+    data = request.form
 
     # Get the image file from the request
     image_file = request.files['image']
 
     # Run the image through the classification model to get the prediction
 
+
+
+    # test data for now
     classification = "healthy"
     severity = 45
 
+    classification_ID= get_classification_id_by_name(classification)
+    # solutions= get_all_solutions_json()
+
     # Create a new Scan object and set its image attribute to the uploaded file
-    scan = create_scan(image=image_file.read(),classification=classification, severity=severity,user_id=data["user_id"])
+    scan = create_scan(image=image_file.read(),classification=classification_ID, severity=severity,user_id=data["user_id"])
     if scan:
         return jsonify(scan.toJSON()), 201
     return jsonify({"error"}), 400
+
+
+@classify_views.route('/recent',method=['GET'])
+def get_recent_scans():
+    scans = get_all_scans_json()
+    return jsonify(scans)
+
 
 
 
